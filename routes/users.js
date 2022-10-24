10a0b4cd4b482355582ee7ada5cdd2c40bff2160
Stas Khvan <hvanstas@gmail.com>
2022-10-24 22:28:46 +0300

    fix: исправления роутов

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,10 @@ const {
   createUser, getUser, getUsers, updateProfile, updateAvatar
 } = require('../controllers/users');
 
-router.get('/', getUsers);
-router.get('/:userId', getUser);
-router.post('/', createUser);
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.get('/users', getUsers);
+router.get('/users/:userId', getUser);
+router.post('/users', createUser);
+router.patch('/users/me', updateProfile);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
